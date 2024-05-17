c9f251aaba65859477a6ed168461bef1731cd09d
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2024-05-17 18:21:39 +1200

    Add files via upload

--- 02_Selecting_Shapes_v1.py
+++ 02_Selecting_Shapes_v1.py
@@ -1,39 +1,28 @@
-# Imports up here...
-
-
-# Functions go here... 
-
-
 # Checks if User's Input was either a Yes or No
 # If User inputs something else, it displays error message
-def yes_no(question):
+def select_shape(question):
 
-    to_check = ["yes", "no"]
+    shapes_list =  ["circle", "square", "triangle", "rectangle"]
 
     while True:
 
         response = input(question).lower()
 
         # If the response is inside the list
-        for var_item in to_check:
+        for var_item in shapes_list:
             # And the response is either the word or the first 
             # letter of the word, returns the entire word
             if response == var_item:
                 return response
             elif response == var_item[0]:
                 return var_item
-        print("Please enter either yes or no...\n")
-
-
-
+            
+        print("Please choose from: Circle (c), Square (s), Triangle (t), or Rectangle (r)\n")
 
 # Main Routine...
         
+# Loop for testing
 while True: 
-    yes_no_instructions = yes_no("Do want to read the instructions? ")
+    chosen_shape = select_shape("Please select a shape (Circle, Square, Triangle, or Rectangle)? ")
     
-    if yes_no_instructions == "yes": 
-        print("Instructions go here...")
-    
-    else:
-        print("Program Continues")
+    print(f"You chose {chosen_shape}")
