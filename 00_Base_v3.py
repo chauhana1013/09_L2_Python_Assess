a3ffa0a947a7af879143d3c06461495b1fe7d8ec
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2024-07-03 18:38:22 +1200

    Outputs Table when Program Ends

--- 00_Base_v3.py
+++ 00_Base_v3.py
@@ -112,7 +112,7 @@ while True:
             # Heron's Law (Calculates the Area of the Triangle using the 3 sides)
             else:    
                 perimeter = side1 + side2 + side3  
-                s = side1 + side2 + side3 / 2        
+                s = (side1 + side2 + side3) / 2          
                 area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
                 lengths_given = f"Three Sides: {side1}, {side2}, {side3}"
 
